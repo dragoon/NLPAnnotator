8c88647e774c444074adada36a431b64784fb5a4
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-03-12 10:39:04 +0100

    added python thrift client

---
+++ src/main/python/com/farawaytech/nlp/dateannotator/thrift/client.py
@@ -0,0 +1,33 @@
+import sys
+
+from . import DateAnnotatorService
+from .ttypes import *
+
+from thrift import Thrift
+from thrift.transport import THttpClient
+#from thrift.transport import TTransport
+from thrift.protocol import TCompactProtocol
+
+try:
+
+    transport = THttpClient.THttpClient("http://srv.exascale.info:8080/NLPAnnotator/dateannotation")
+    # Buffering is critical. Raw sockets are very slow
+    #transport = TTransport.TBufferedTransport(transport)
+    protocol = TCompactProtocol.TCompactProtocol(transport)
+    client = DateAnnotatorService.Client(protocol)
+    # Connect!
+    transport.open()
+
+    for line in sys.stdin:
+        sentence = line.strip()
+        response = client.annotate(sentence, None)
+        for time_annotation in response.annotations:
+            print time_annotation.startToken
+            print time_annotation.endToken
+            print time_annotation.temporal
+
+    # Close!
+    transport.close()
+
+except Thrift.TException, tx:
+    print '%s' % (tx.message)
