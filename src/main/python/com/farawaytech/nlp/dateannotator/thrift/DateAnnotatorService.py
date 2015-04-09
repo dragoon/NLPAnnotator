186060b2c6997ea55d6066ab302ca2c1630dda36
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-04-09 15:35:47 +0200

    update python thrift

--- src/main/python/com/farawaytech/nlp/dateannotator/thrift/DateAnnotatorService.py
+++ src/main/python/com/farawaytech/nlp/dateannotator/thrift/DateAnnotatorService.py
@@ -21,8 +21,19 @@ class Iface(object):
   def annotate(self, sentence, date):
     """
     Extracts date annotations from a given sentence/string.
-    ranking function. The sentence has to be already properly tokenized, only whitespace split will be applied.
+    The sentence has to be already properly tokenized, only whitespace split will be applied.
     Date argument is required for relative dates, otheriwise current date will be applied.
+
+    Parameters:
+     - sentence
+     - date
+    """
+    pass
+
+  def annotateInline(self, sentence, date):
+    """
+    Performs inline datetime annotations on a given sentence.
+
 
     Parameters:
      - sentence
@@ -41,7 +52,7 @@ class Client(Iface):
   def annotate(self, sentence, date):
     """
     Extracts date annotations from a given sentence/string.
-    ranking function. The sentence has to be already properly tokenized, only whitespace split will be applied.
+    The sentence has to be already properly tokenized, only whitespace split will be applied.
     Date argument is required for relative dates, otheriwise current date will be applied.
 
     Parameters:
@@ -75,12 +86,49 @@ class Client(Iface):
       return result.success
     raise TApplicationException(TApplicationException.MISSING_RESULT, "annotate failed: unknown result");
 
+  def annotateInline(self, sentence, date):
+    """
+    Performs inline datetime annotations on a given sentence.
+
+
+    Parameters:
+     - sentence
+     - date
+    """
+    self.send_annotateInline(sentence, date)
+    return self.recv_annotateInline()
+
+  def send_annotateInline(self, sentence, date):
+    self._oprot.writeMessageBegin('annotateInline', TMessageType.CALL, self._seqid)
+    args = annotateInline_args()
+    args.sentence = sentence
+    args.date = date
+    args.write(self._oprot)
+    self._oprot.writeMessageEnd()
+    self._oprot.trans.flush()
+
+  def recv_annotateInline(self):
+    iprot = self._iprot
+    (fname, mtype, rseqid) = iprot.readMessageBegin()
+    if mtype == TMessageType.EXCEPTION:
+      x = TApplicationException()
+      x.read(iprot)
+      iprot.readMessageEnd()
+      raise x
+    result = annotateInline_result()
+    result.read(iprot)
+    iprot.readMessageEnd()
+    if result.success is not None:
+      return result.success
+    raise TApplicationException(TApplicationException.MISSING_RESULT, "annotateInline failed: unknown result");
+
 
 class Processor(Iface, TProcessor):
   def __init__(self, handler):
     self._handler = handler
     self._processMap = {}
     self._processMap["annotate"] = Processor.process_annotate
+    self._processMap["annotateInline"] = Processor.process_annotateInline
 
   def process(self, iprot, oprot):
     (name, type, seqid) = iprot.readMessageBegin()
@@ -104,6 +152,17 @@ class Processor(Iface, TProcessor):
     result = annotate_result()
     result.success = self._handler.annotate(args.sentence, args.date)
     oprot.writeMessageBegin("annotate", TMessageType.REPLY, seqid)
+    result.write(oprot)
+    oprot.writeMessageEnd()
+    oprot.trans.flush()
+
+  def process_annotateInline(self, seqid, iprot, oprot):
+    args = annotateInline_args()
+    args.read(iprot)
+    iprot.readMessageEnd()
+    result = annotateInline_result()
+    result.success = self._handler.annotateInline(args.sentence, args.date)
+    oprot.writeMessageBegin("annotateInline", TMessageType.REPLY, seqid)
     result.write(oprot)
     oprot.writeMessageEnd()
     oprot.trans.flush()
@@ -230,6 +289,148 @@ class annotate_result(object):
     if self.success is not None:
       oprot.writeFieldBegin('success', TType.STRUCT, 0)
       self.success.write(oprot)
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    return
+
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.success)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
+class annotateInline_args(object):
+  """
+  Attributes:
+   - sentence
+   - date
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.STRING, 'sentence', None, None, ), # 1
+    (2, TType.STRING, 'date', None, None, ), # 2
+  )
+
+  def __init__(self, sentence=None, date=None,):
+    self.sentence = sentence
+    self.date = date
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.STRING:
+          self.sentence = iprot.readString().decode('utf-8')
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.STRING:
+          self.date = iprot.readString().decode('utf-8')
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('annotateInline_args')
+    if self.sentence is not None:
+      oprot.writeFieldBegin('sentence', TType.STRING, 1)
+      oprot.writeString(self.sentence.encode('utf-8'))
+      oprot.writeFieldEnd()
+    if self.date is not None:
+      oprot.writeFieldBegin('date', TType.STRING, 2)
+      oprot.writeString(self.date.encode('utf-8'))
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    return
+
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.sentence)
+    value = (value * 31) ^ hash(self.date)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
+class annotateInline_result(object):
+  """
+  Attributes:
+   - success
+  """
+
+  thrift_spec = (
+    (0, TType.STRING, 'success', None, None, ), # 0
+  )
+
+  def __init__(self, success=None,):
+    self.success = success
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 0:
+        if ftype == TType.STRING:
+          self.success = iprot.readString().decode('utf-8')
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('annotateInline_result')
+    if self.success is not None:
+      oprot.writeFieldBegin('success', TType.STRING, 0)
+      oprot.writeString(self.success.encode('utf-8'))
       oprot.writeFieldEnd()
     oprot.writeFieldStop()
     oprot.writeStructEnd()
